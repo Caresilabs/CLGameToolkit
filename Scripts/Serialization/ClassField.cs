444ff9c2f2bd72bf21051804906a2a62c4595940
Simon Bothén <caresilabs@gmail.com>
2025-06-02 12:01:54 +0200

    feat: Add save feature

---
+++ Scripts/Serialization/ClassField.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System;
+using System.Reflection;
+
+namespace CLGameToolkit.Serialization
+{
+    [Serializable]
+    public class ClassField<T>
+    {
+        public T Target { get; set; }
+        public string FieldName;
+
+        public Action<ClassField<T>> OnValueChanged;
+
+        public R GetValue<R>()
+        {
+            return GetValue<R>(Target);
+        }
+
+        public R GetValue<R>(T overrideTarget)
+        {
+            var field = GetFieldInfo();
+
+            if (field == null)
+                return default;
+
+            var value = field.GetValue(overrideTarget);
+
+            if (typeof(R) == typeof(int) && value is bool boolVal) // Request convertion to int
+            {
+                return (R)((object)(boolVal ? 1 : 0));
+            }
+
+            return (R)value;
+        }
+
+        public void SetValue<R>(R value)
+        {
+            var field = GetFieldInfo();
+
+            if (field.FieldType == typeof(bool) && value is int intVal) // Request convertion to int
+            {
+                field.SetValue(Target, intVal >= 1);
+            }
+            else
+            {
+                field.SetValue(Target, value);
+            }
+
+            OnValueChanged?.Invoke(this);
+        }
+
+        public FieldInfo GetFieldInfo()
+        {
+            return typeof(T).GetField(FieldName,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        }
+
+        public bool IsName(string fieldCompare)
+        {
+            return FieldName == fieldCompare;
+        }
+    }
+
+
+#if UNITY_EDITOR
+
+    [UnityEditor.CustomPropertyDrawer(typeof(ClassField<>), true)]
+    public class ClassFieldDrawer : UnityEditor.PropertyDrawer
+    {
+        public override void OnGUI(Rect position, UnityEditor.SerializedProperty property, GUIContent label)
+        {
+            Type referenceType = fieldInfo.FieldType.IsArray ? fieldInfo.FieldType.GetElementType() : fieldInfo.FieldType;
+            Type genericType = referenceType.GetGenericArguments()?[0];
+
+            UnityEditor.SerializedProperty fieldNameProp = property.FindPropertyRelative("FieldName");
+
+            FieldInfo[] fields = genericType.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            string[] options = Array.ConvertAll(fields, f => f.Name);
+
+            int selectedIndex = Array.FindIndex(options, name => name == fieldNameProp.stringValue);
+
+            if (string.IsNullOrEmpty(fieldNameProp.stringValue))
+            {
+                fieldNameProp.stringValue = options.Length > 0 ? options[0] : "";
+                return;
+            }
+
+            if (selectedIndex < 0)
+            {
+                Logger.Error($"Field<{genericType.Name}> fieldname (\"{fieldNameProp.stringValue}\") is now invalid", property.serializedObject.targetObject);
+                UnityEditor.EditorGUILayout.LabelField("Error! Update field name!");
+                selectedIndex = 0;
+            }
+
+            UnityEditor.EditorGUI.BeginProperty(position, label, property);
+
+            int newSelectedIndex = UnityEditor.EditorGUILayout.Popup($"Field<{genericType.Name}>", selectedIndex, options);
+
+            if (newSelectedIndex != selectedIndex)
+                fieldNameProp.stringValue = options.Length > 0 ? options[newSelectedIndex] : "";
+
+            UnityEditor.EditorGUI.EndProperty();
+        }
+    }
+
+#endif
+}
---
+++ Scripts/Serialization/SaveHandler.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace CLGameToolkit.Serialization
+{
+    public class SaveHandler<THeader, TGameFile> where THeader : SaveFileBase, new() where TGameFile : SaveFileBase, new() // : MonoBehaviour
+    {
+
+        public THeader Header { get; private set; }
+        public TGameFile CurrentLoadedGame { get; private set; }
+
+        public bool BackupOnNewGame { get; set; } = true;
+        public ITextTransformer TextTransformer { get; set; }
+
+        private string FilePath;
+        private int profileIndex = 0;
+        private bool playerRequestedNewGame;
+
+        private readonly string headerProfileName;
+        private readonly Func<int, string> gameProfileNameGen;
+
+        public SaveHandler(string headerProfileName, Func<int, string> gameProfileNameGen)
+        {
+            this.headerProfileName = headerProfileName;
+            this.gameProfileNameGen = gameProfileNameGen;
+            this.TextTransformer = new DefaultTextTransformer();
+
+            InitPaths();
+            Load();
+        }
+
+        private void InitPaths()
+        {
+            FilePath = Path.Combine(Application.persistentDataPath, "saves");
+
+            if (!Directory.Exists(FilePath)) Directory.CreateDirectory(FilePath);
+        }
+
+        private void Load()
+        {
+            Header = ReadFromFile<THeader>(headerProfileName) ?? new THeader().Defaults() as THeader;
+            Header = Migrate(Header);
+        }
+
+        public void SaveHeader()
+        {
+            SaveFile(headerProfileName, Header);
+        }
+
+        public void SaveGameProfile()
+        {
+            playerRequestedNewGame = false;
+            SaveFile(gameProfileNameGen(profileIndex), CurrentLoadedGame);
+            Logger.Notice("Saved Game!");
+        }
+
+        public void SelectGameProfile(int profile, bool newGame = false)
+        {
+            profileIndex = profile;
+            playerRequestedNewGame = newGame;
+        }
+
+        public bool HasSaveFile()
+        {
+            return File.Exists(Path.Combine(FilePath, gameProfileNameGen(profileIndex)));
+        }
+
+        public void LoadGameProfile()
+        {
+            if (playerRequestedNewGame)
+            {
+                TGameFile oldGame = ReadFromFile<TGameFile>(gameProfileNameGen(profileIndex));
+                if (oldGame != null && BackupOnNewGame) // Backup old file
+                    SaveFile($"{profileIndex}.save_backup{DateTime.UtcNow:yyyyMMddTHHmmss}", oldGame); // TODO Adjust naming
+
+                CurrentLoadedGame = new TGameFile().Defaults() as TGameFile;
+                return;
+            }
+
+            CurrentLoadedGame = ReadFromFile<TGameFile>(gameProfileNameGen(profileIndex)) ?? new TGameFile().Defaults() as TGameFile;
+            CurrentLoadedGame = Migrate(CurrentLoadedGame);
+        }
+
+        public void ResetAll()
+        {
+            InitPaths();
+
+            SaveFile(headerProfileName, null);
+            SaveFile(gameProfileNameGen(profileIndex), null);
+        }
+
+
+        private T ReadFromFile<T>(string fileName) where T : SaveFileBase
+        {
+            string path = Path.Combine(FilePath, fileName);
+
+            if (!File.Exists(path))
+                return default;
+
+            string dataString = File.ReadAllText(path);
+            return JsonUtility.FromJson<T>(TextTransformer.OnDeserialize(dataString));
+        }
+
+        private void SaveFile(string fileName, object file)
+        {
+            string path = Path.Combine(FilePath, fileName);
+            string tmpFilePath = path + ".tmp";
+
+            string dataString = TextTransformer.OnSerialize(JsonUtility.ToJson(file, true));
+
+            if (!File.Exists(path))
+            {
+                File.WriteAllText(path, dataString);
+                return;
+            }
+
+            // Do safe swap to avoid corrupted files
+            File.WriteAllText(tmpFilePath, dataString);
+            File.Replace(tmpFilePath, path, null);
+            File.Delete(tmpFilePath);
+        }
+
+        // TODO: Abstract
+        private T Migrate<T>(T file) where T : SaveFileBase
+        {
+            string current = Application.version;
+            string old = file.AppVersion;
+
+            if (string.IsNullOrEmpty(old) || old == current) return file;
+
+            Version oldVersion = new(old);
+
+            file.AppVersion = current;
+            return file;
+        }
+
+    }
+
+    public interface ITextTransformer
+    {
+        string OnSerialize(string text);
+        string OnDeserialize(string text);
+    }
+
+    internal struct DefaultTextTransformer : ITextTransformer
+    {
+        public readonly string OnDeserialize(string text) => text;
+
+        public readonly string OnSerialize(string text) => text;
+    }
+
+    public abstract class SaveFileBase
+    {
+        [SerializeField] public string AppVersion;
+
+        public abstract SaveFileBase Defaults();
+    }
+}
