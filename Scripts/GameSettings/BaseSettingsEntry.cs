d1983cae4aee0c4539c2fdcde5205c20288ed0fb
Simon Bothén <caresilabs@gmail.com>
2025-06-02 12:02:21 +0200

    feat: Add SettingsManager

---
+++ Scripts/GameSettings/BaseSettingsEntry.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using CLGameToolkit.Serialization;
+using CLGameToolkit.UI;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CLGameToolkit.GameSettings
+{
+    public abstract class BaseSettingsEntry<T> : MonoBehaviour
+    {
+        [SerializeField] private ClassField<T> Field;
+
+        private SettingsManager<T> manager;
+
+        private Slider slider;
+        private Toggle toggle;
+        private ToggleButtonGroup toggleGroup;
+        private TMP_Dropdown dropdown;
+
+        // TODO: Optimize to not remove listener
+        // TODO: Make Manager mandatory in validate
+        private void OnEnable()
+        {
+            if (manager == null)
+            {
+                manager = GetComponentInParent<SettingsManager<T>>();
+                Field.OnValueChanged += manager.NotifyFieldUpdate;
+            }
+
+            Field.Target = manager.Current;
+
+            if (slider != null || TryGetComponent(out slider))
+            {
+                slider.SetValueWithoutNotify(Field.GetValue<float>());
+                slider.onValueChanged.RemoveAllListeners();
+                slider.onValueChanged.AddListener(Field.SetValue);
+                return;
+            }
+
+            if (toggle != null || TryGetComponent(out toggle))
+            {
+                toggle.SetIsOnWithoutNotify(Field.GetValue<bool>());
+                toggle.onValueChanged.RemoveAllListeners();
+                toggle.onValueChanged.AddListener(Field.SetValue);
+                return;
+            }
+
+
+            Type fieldType = Field.GetFieldInfo().FieldType;
+            if (fieldType.IsEnum)
+            {
+                int startValue = Field.GetValue<int>();
+
+                int[] enumValues = Enum.GetValues(fieldType).Cast<int>().OrderBy(x => x).ToArray(); // TODO: A bit slow, and the ::GetValues function sorts them in (u_int32) 
+                startValue = Array.IndexOf(enumValues, startValue); // When using any enum, we need to convert it to an index
+
+                if (dropdown != null || TryGetComponent(out dropdown))
+                {
+                    dropdown.onValueChanged.RemoveAllListeners();
+                    dropdown.onValueChanged.AddListener((index) => Field.SetValue(enumValues[index]));
+                    dropdown.SetValueWithoutNotify(startValue);
+                }
+                else if (toggleGroup != null || TryGetComponent(out toggleGroup))
+                {
+                    toggleGroup.OnValueChanged.RemoveAllListeners();
+                    toggleGroup.OnValueChanged.AddListener((index) => Field.SetValue(enumValues[index]));
+                    toggleGroup.SetValueWithoutNotify(startValue);
+                }
+                return;
+            }
+
+
+            if (dropdown != null || TryGetComponent(out dropdown))
+            {
+                dropdown.onValueChanged.RemoveAllListeners();
+                dropdown.onValueChanged.AddListener(Field.SetValue);
+                dropdown.SetValueWithoutNotify(Field.GetValue<int>());
+                return;
+            }
+
+            if (toggleGroup != null || TryGetComponent(out toggleGroup))
+            {
+                toggleGroup.SetValueWithoutNotify(Field.GetValue<int>());
+                toggleGroup.OnValueChanged.RemoveAllListeners();
+                toggleGroup.OnValueChanged.AddListener(Field.SetValue);
+                return;
+            }
+        }
+    }
+}
---
+++ Scripts/GameSettings/SettingsManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+using CLGameToolkit.Serialization;
+using UnityEngine;
+
+namespace CLGameToolkit.GameSettings
+{
+    public abstract class SettingsManager<T> : MonoBehaviour
+    {
+        public static Action<T> OnSettingsSaved;
+        public T Current { get; private set; }
+
+        [SerializeField] private CanvasGroup SettingsGroup;
+        [SerializeField] protected bool RequireConfirm;
+
+        public void Open()
+        {
+            Current = CloneSettings();
+            SettingsGroup.gameObject.SetActive(true);
+        }
+
+        public void Close()
+        {
+            SettingsGroup.gameObject.SetActive(false);
+
+            if (!RequireConfirm)
+                ApplySettings();
+        }
+
+        public void ApplySettings()
+        {
+            if (SaveSettings())
+                OnSettingsSaved?.Invoke(Current);
+        }
+
+        protected bool IsChanged(T other)
+        {
+            foreach (var field in typeof(T).GetFields(BindingFlags.Instance | BindingFlags.Public))
+            {
+                var value1 = field.GetValue(Current);
+                var value2 = field.GetValue(other);
+                if (!value1.Equals(value2))
+                    return true;
+            }
+            foreach (var prop in typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public))
+            {
+                var value1 = prop.GetValue(Current);
+                var value2 = prop.GetValue(other);
+                if (!value1.Equals(value2))
+                    return true;
+            }
+            return false;
+        }
+
+        public void NotifyFieldUpdate(ClassField<T> field)
+        {
+            if (RequireConfirm)
+                return;
+
+            OnFieldUpdated(field);
+        }
+
+        protected abstract bool SaveSettings();
+        protected abstract T CloneSettings();
+        protected abstract void OnFieldUpdated(ClassField<T> field);
+    }
+}
