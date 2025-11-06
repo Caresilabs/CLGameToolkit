b576e4d84f1ddf6c6851b98b8f62edda216dac51
Simon Bothén <caresilabs@gmail.com>
2025-11-06 19:55:53 +0100

    Add ClassReference and SingleFlagAttribute

---
+++ Scripts/Attributes/SingleFlagAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace CLGameToolkit.Attributes
+{
+    public class SingleFlagAttribute : PropertyAttribute { }
+   
+#if UNITY_EDITOR
+
+    [UnityEditor.CustomPropertyDrawer(typeof(SingleFlagAttribute))]
+    public class SingleEnumFlagSelectAttributeEditor : UnityEditor.PropertyDrawer
+    {
+        public override void OnGUI(Rect position, UnityEditor.SerializedProperty property, GUIContent label)
+        {
+            Type enumType = fieldInfo.FieldType;
+
+            if (!enumType.IsEnum && enumType != typeof(int))
+            {
+                UnityEditor.EditorGUI.LabelField(position, label.text, "Use SingleEnumFlagSelect with int or enum.");
+                return;
+            }
+
+            var displayTexts = new System.Collections.Generic.List<GUIContent>();
+            var enumValues = new System.Collections.Generic.List<int>();
+
+            foreach (var displayText in Enum.GetValues(enumType))
+            {
+                displayTexts.Add(new GUIContent(displayText.ToString()));
+                enumValues.Add((int)displayText);
+            }
+
+            property.intValue = UnityEditor.EditorGUI.IntPopup(position, label, property.intValue, displayTexts.ToArray(), enumValues.ToArray());
+        }
+    }
+
+#endif
+
+}
---
+++ Scripts/Serialization/ClassReference.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace CLGameToolkit.Serialization
+{
+    [Serializable]
+    public class ClassReference<T>
+    {
+        [SerializeField] private string ClassTypeName;
+
+        public Type ClassType
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ClassTypeName)) return null;
+                return Type.GetType(ClassTypeName);
+            }
+        }
+
+        public void SetClassType(Type type)
+        {
+            if (type == null || !typeof(T).IsAssignableFrom(type))
+            {
+                Debug.LogError($"Invalid type assignment. {type?.Name} is not assignable to {typeof(T).Name}");
+                return;
+            }
+            ClassTypeName = type.AssemblyQualifiedName;
+        }
+
+        public T CreateInstance()
+        {
+            if (ClassType == null)
+                return default;
+
+            return (T)Activator.CreateInstance(ClassType);
+        }
+    }
+
+
+#if UNITY_EDITOR
+
+    [UnityEditor.CustomPropertyDrawer(typeof(ClassReference<>), true)]
+    public class ClassReferenceDrawer : UnityEditor.PropertyDrawer
+    {
+        public override void OnGUI(Rect position, UnityEditor.SerializedProperty property, GUIContent label)
+        {
+            UnityEditor.SerializedProperty classTypeNameProp = property.FindPropertyRelative("ClassTypeName");
+
+            Type fieldType = fieldInfo.FieldType;
+            Type baseType = fieldType.GetGenericArguments()[0];
+
+            List<Type> derivedTypes = GetAllDerivedTypes(baseType);
+            string[] displayNames = derivedTypes.Select(t => t.FullName).ToArray();
+
+            int currentIndex = 0;
+            if (!string.IsNullOrEmpty(classTypeNameProp.stringValue))
+            {
+                var currentType = Type.GetType(classTypeNameProp.stringValue);
+                currentIndex = derivedTypes.IndexOf(currentType);
+                if (currentIndex < 0) currentIndex = 0;
+            }
+
+            UnityEditor.EditorGUI.BeginProperty(position, label, property);
+            int newIndex = UnityEditor.EditorGUI.Popup(position, $"{baseType.Name} Type", currentIndex, displayNames);
+            if (newIndex != currentIndex && newIndex >= 0 && newIndex < derivedTypes.Count)
+            {
+                classTypeNameProp.stringValue = derivedTypes[newIndex].AssemblyQualifiedName;
+            }
+
+            UnityEditor.EditorGUI.EndProperty();
+        }
+
+        private static List<Type> GetAllDerivedTypes(Type baseType)
+        {
+            var result = new List<Type>();
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            foreach (var asm in assemblies)
+            {
+                Type[] types;
+                try { types = asm.GetTypes(); }
+                catch (ReflectionTypeLoadException e) { types = e.Types.Where(t => t != null).ToArray(); }
+
+                foreach (var t in types)
+                {
+                    if (t == null || t.IsAbstract) continue;
+
+                    if (baseType.IsInterface)
+                    {
+                        if (t.GetInterfaces().Contains(baseType))
+                            result.Add(t);
+                    }
+                    else if (t.IsSubclassOf(baseType))
+                    {
+                        result.Add(t);
+                    }
+                    else if (t == baseType)
+                    {
+                        result.Add(t);
+                    }
+                }
+            }
+
+            return result.OrderBy(t => t.Name).ToList();
+        }
+    }
+}
+#endif
